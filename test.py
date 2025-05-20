2eb04c5004b9d9be345d5f4186df9ffa64b061d2
Puchala1036 <102521762+Puchala1036@users.noreply.github.com>
2025-05-20 16:09:36 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+#Mathematical Operations
+
+n1=input(" enter the number1: ")
+n2=input(" enter the number2: ")
+n1=int(n1)
+n2=int(n2)
+n3=n1+n2
+n4=n1-n2
+n5=n1*n2
+n6=n1/n2
+print("Addition:",  n3 )
+print("Subtraction: ", n4 )
+print("Multiplication: ", n5  )
+print("Division: ", n6 )
+
+#Personalized Greeting
+input1=input(" first name ")
+input2=input(" last name ")
+full_Name=input1+input2
+print(" Hello, ", full_Name,"!" ," Welcome to the python program ")

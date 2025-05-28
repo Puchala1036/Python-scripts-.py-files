68c60e91c9fc91ee1a7a604dbdfe5b6df62dfab5
Puchala1036 <102521762+Puchala1036@users.noreply.github.com>
2025-05-28 14:46:16 +0530

    Create Data Structures and Strings in Python

--- test1.py
+++ test1.py
@@ -1,19 +1,22 @@
-#Handle Errors 
-with open('samp.txt', 'r') as file:
-        for line in file:
-            print(line, end='')
-except FileNotFoundError:
-print('Error: The file sample.txt does not exist.')
+#Create a Dictionary of Student Marks
+'''
+dict={"sss":98,"hhh":56,"wew":98 }
+ask_studName=input("Enter the student's name:")
+for i in dict:
+    if ask_studName ==i:
+        print("{} marks:{}".format(ask_studName,dict[i]))
+        found=True
+        break 
+    else:
+        print("Student not found {}".format(ask_studName))
+'''
+#Demonstrate List Slicing 
 
-
- 
-
-#Append Data to a File
-file1=open('sam.txt','a')
-append_file=file1.write('\n Learning file  Handling in Python')
-print(append_file)
-file1.close()
-file1=open('sam.txt','r')
-append_file=file1.read()
-print(append_file)
-file1.close()
+'''
+Original_List=[1,2,3,4,5,6,7,8,9,10]
+Extracted_List=(Original_List[0:5])
+print("Extracted first five elements", Extracted_List)
+Reversed_List=Extracted_List.copy()
+Reversed_List.reverse()
+print("Reversed Extracted elements", Reversed_List)
+'''
